0e76b7639651205c7ed2dcf1d26f728813c678ca
Ronith Ganjigunta <68761284+ronithgan@users.noreply.github.com>
2022-08-09 12:26:59 +0530

    Update store.js

--- store.js
+++ store.js
@@ -30,32 +30,3 @@ var searchdiv = document.querySelector(".css-14b3tu7-HomeSearch");
                 
 			}
 		}
-
-        //replace scroll down with choose stores
-
-
-        
-
-        /*
-var searchdivnew = document.getElementById("search-term-accessibility-navigation");
-		if (searchdivnew) {
-            console.log("found search term...")
-			var input16 = searchdivnew.parentElement;
-			if (input16) {
-				var input15 = input16.parentElement;
-
-				var img8 = document.createElement('img');
-				img8.src= "https://bingoimagesv1.s3-us-west-1.amazonaws.com/search_2.png";
-				img8.style.marginLeft = '0%';
-				//img8.style.marginTop = '10%';
-				img8.style.width = "auto";
-				img8.style.height = "60px";
-				//input15.insertBefore(img8, input15);
-				input16.appendChild(img8);
-			}
-		}
-
-      */  
-
-        
-//search-term-accessibility-navigation
